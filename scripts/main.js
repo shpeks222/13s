444ce0d8bf20413d5a033ffcffa79556e94a4914
shpeks222 <shpeks22@yandex.ru>
2022-01-26 22:49:12 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+let scet_one = 0;
+document.querySelector(".Pviwe").innerText = scet_one;
+function sum() {
+    scet_one = scet_one + 1;
+  document.querySelector(".Pviwe").innerText = scet_one;
+}
+
+let scet_two = 0;
+document.querySelector(".Pviwe2").innerText = scet_two;
+function sum2() {
+    scet_two = scet_two + 1;
+  document.querySelector(".Pviwe2").innerText = scet_two;
+}
+
+let scet_there = 0;
+document.querySelector(".Pviwe3").innerText = scet_there;
+function sum3() {
+    scet_there = scet_there + 1;
+  document.querySelector(".Pviwe3").innerText = scet_there;
+}
+
+let scet_four = 0;
+document.querySelector(".Pviwe4").innerText = scet_four;
+function sum4() {
+    scet_four = scet_four + 1;
+  document.querySelector(".Pviwe4").innerText = scet_four;
+}
+
