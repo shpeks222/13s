96e3c022fe036fa04cc75a48aa8aa8027c9d379f
shpeks222 <shpeks22@yandex.ru>
2022-01-27 10:41:16 +0300

    updated nama ja function

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
 let scet_one = 0;
-document.querySelector(".newsOneClkickCounter").innerText = scet_one;
-function sum() {
+document.querySelector(".newsOneClickCounter").innerText = scet_one;
+function newsOneClickCounter() {
     scet_one = scet_one + 1;
-  document.querySelector(".newsOneClkickCounter").innerText = scet_one;
+  document.querySelector(".newsOneClickCounter").innerText = scet_one;
 }
 
 let scet_two = 0;
-document.querySelector(".newsTwoClkickCounter").innerText = scet_two;
-function sum2() {
+document.querySelector(".newsTwoClickCounter").innerText = scet_two;
+function newsTwoClickCounter() {
     scet_two = scet_two + 1;
-  document.querySelector(".newsTwoClkickCounter").innerText = scet_two;
+  document.querySelector(".newsTwoClickCounter").innerText = scet_two;
 }
 
 let scet_there = 0;
-document.querySelector(".newsThereClkickCounter").innerText = scet_there;
-function sum3() {
+document.querySelector(".newsThereClickCounter").innerText = scet_there;
+function newsThereClickCounter() {
     scet_there = scet_there + 1;
-  document.querySelector(".newsThereClkickCounter").innerText = scet_there;
+  document.querySelector(".newsThereClickCounter").innerText = scet_there;
 }
 
 let scet_four = 0;
-document.querySelector(".newsFourClkickCounter").innerText = scet_four;
-function sum4() {
+document.querySelector(".newsFourClickCounter").innerText = scet_four;
+function newsFourClickCounter() {
     scet_four = scet_four + 1;
-  document.querySelector(".newsFourClkickCounter").innerText = scet_four;
+  document.querySelector(".newsFourClickCounter").innerText = scet_four;
 }
 
